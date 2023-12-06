ae38fb6de88a3262c438b34c4833a87abf71037f
Jashanpreet Kaur <multanijashan1810@gmail.com>
2023-12-06 19:41:29 +0530

    calculations

---
+++ calculations.js
@@ -0,0 +1,8 @@
+let num1 = 20;
+let num2 = 10;
+const addition = num1+num2;
+const subtraction = num1-num2;
+const multiplication = num1*num2;
+console.log(addition)
+console.log(subtraction)
+console.log(multiplication)
